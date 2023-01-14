8a9bb7ec26fc4ba26230babb45fc8f97552a8e3c
Mansurcan Atmaca <mansur-atmaca@hotmail.com>
2023-01-14 18:59:53 +0300

    pizza order end

--- pizza_order.dart
+++ pizza_order.dart
@@ -1,22 +1,26 @@
-import 'dart:math';
-
 void main(List<String> args) {
-  const pizzaPrice={
-    'margherita':20,
-    'pepperoni':30,
-    'vegetarian':40,
-    'hawaian':45,
+  const order = ['vegetarian', 'pepperoni'];
 
+  var totalPrice = pizzaCheck(order);
+  print("Rotal Price: $totalPrice");
+}
+
+double pizzaCheck(List orderList) {
+  const pizzaPrice = {
+    'margherita': 20,
+    'pepperoni': 25,
+    'vegetarian': 52,
+    'hawaian': 45,
   };
-  const order=['hawaian','margherita'];
-  var total=0.0;
-  for(var pizza in order){
-    final price=pizzaPrice[pizza];
 
-    if(price!=null){
-total+=price;
+  var total = 0.0;
+
+  for (var pizza in orderList) {
+    final price = pizzaPrice[pizza];
+
+    if (price != null) {
+      total += price;
     }
-
   }
-  print("Total Price: $total");
+  return total;
 }
