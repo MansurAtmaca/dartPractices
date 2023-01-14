ff54c59e304c5463d2b85c0db17168c211953064
Mansurcan Atmaca <mansur-atmaca@hotmail.com>
2023-01-14 17:40:29 +0300

    pizza order added

--- pizza_order.dart
+++ pizza_order.dart
@@ -1,5 +1,22 @@
+import 'dart:math';
+
 void main(List<String> args) {
   const pizzaPrice={
+    'margherita':20,
+    'pepperoni':30,
+    'vegetarian':40,
+    'hawaian':45,
 
   };
+  const order=['hawaian','margherita'];
+  var total=0.0;
+  for(var pizza in order){
+    final price=pizzaPrice[pizza];
+
+    if(price!=null){
+total+=price;
+    }
+
+  }
+  print("Total Price: $total");
 }
