e3a36b2a823893980eea66af390e518d0f25f24e
Mansurcan Atmaca <mansur-atmaca@hotmail.com>
2023-01-16 19:08:03 +0300

    where funtion added

--- main.dart
+++ main.dart
@@ -1,9 +1,7 @@
-
 import 'pizza_order.dart';
 
-
 void main(List<String> args) {
- /*
+  /*
  Pizza order practice in pizza_order.dart
 
    const order = ['vegetarian', 'pepperoni'];
@@ -14,7 +12,22 @@ void main(List<String> args) {
 
   */
 
-const list=[1,2,3,4];
-list.forEach(print);
+  const numList=<int>[2,4,6,8,10,16,20,24,30,45];
+
+  final listo=where(numList, (value)=>value%5==0);
+
+print(listo);
+}
+
+List<T> where<T>(List<T> list,bool Function(T) f){
+var newlist=<T>[];
+for(var i in list){
+  if(f(i)){
+    newlist.add(i);
+  }
+}
+return newlist;
+
+
 
 }
